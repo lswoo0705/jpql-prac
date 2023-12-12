296ec9631c3c284e8035ef168ffb1ac6f3682b36
sangwoo lee <lswoo0705@gmail.com>
2023-12-12 19:20:24 +0900

    jpql practice

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,32 @@
+package jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit(); // 변경 내용을 db에 반영(플러시) -> 이 때 쿼리가 날아감
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
