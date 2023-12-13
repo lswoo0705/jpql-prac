bc4cf0cf377167d74beedf526079a5a40c48b93f
sangwoo lee <lswoo0705@gmail.com>
2023-12-13 18:10:09 +0900

    Projection(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -3,6 +3,7 @@ package jpql;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,10 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+//            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(10);
+//            em.persist(member);
 
             // 반환 타입이 명확할 때
 //            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
@@ -41,10 +42,58 @@ public class JpaMain {
             // 결과가 둘 이상이면 -> NonUniqueResultException
 
             // 파라미터 바인딩
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
+//            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+//                    .setParameter("username", "member1")
+//                    .getSingleResult();
+//            System.out.println("result = " + result.getUsername());
+
+
+            // 프로젝션(SELECT)
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
+//
+//            Member findmember = result.get(0);
+//            findmember.setAge(20);
+
+//            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+//                    .getResultList();
+
+//            em.createQuery("select o.address from Order o", Address.class)
+//                    .getResultList();
+
+//            em.createQuery("select m.username, m.age from Member m")
+//                    .getResultList();
+
+//            List resultList = em.createQuery("select m.username, m.age from Member m")
+//                    .getResultList();
+//
+//            Object o = resultList.get(0);
+//            Object[] result = (Object[]) o;
+//            System.out.println("name = " + result[0]);
+//            System.out.println("age = " + result[1]);
+
+//            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m")
+//                    .getResultList();
+//
+//            Object[] result = resultList.get(0);
+//            System.out.println("name = " + result[0]);
+//            System.out.println("age = " + result[1]);
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.username = " + memberDTO.getUsername());
+            System.out.println("memberDTO.age = " + memberDTO.getAge());
+
 
             tx.commit(); // 변경 내용을 db에 반영(플러시) -> 이 때 쿼리가 날아감
         } catch (Exception e) {
