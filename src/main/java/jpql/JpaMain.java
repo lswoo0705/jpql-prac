9b49d467e5775689951413da113dc23fc11ede74
sangwoo lee <lswoo0705@gmail.com>
2023-12-14 17:12:35 +0900

    Use Entity directly

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -324,6 +324,57 @@ public class JpaMain {
 
 
             // 페치 조인 - 기본
+//            Team teamA = new Team();
+//            teamA.setName("팀A");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("팀B");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("회원1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("회원2");
+//            member2.setTeam(teamA);
+//            em.persist(member2);
+//
+//            Member member3 = new Member();
+//            member3.setUsername("회원3");
+//            member3.setTeam(teamB);
+//            em.persist(member3);
+//
+//            em.flush();
+//            em.clear();
+//
+////            String query = "select m from Member m";
+//            // 회원1, 팀A(SQL)
+//            // 회원2, 팀A(1차캐시)
+//            // 회원3, 팀B(SQL)
+//            // 회원 100명 -> N + 1
+//
+//            // fetch join으로 해결
+////            String query = "select m from Member m join fetch m.team";
+//
+//            // 컬렉션 페치 조인
+//            String query = "select t from Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            System.out.println("result = " + result.size());
+//
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+//                for (Member member : team.getMembers()) {
+//                    System.out.println("member = " + member);
+//                }
+//            }
+
+            // Entity를 직접 사용
             Team teamA = new Team();
             teamA.setName("팀A");
             em.persist(teamA);
@@ -350,29 +401,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-//            String query = "select m from Member m";
-            // 회원1, 팀A(SQL)
-            // 회원2, 팀A(1차캐시)
-            // 회원3, 팀B(SQL)
-            // 회원 100명 -> N + 1
+            String query = "select m from Member m where m = :member";
 
-            // fetch join으로 해결
-//            String query = "select m from Member m join fetch m.team";
+            Member findMember = em.createQuery(query, Member.class)
+                    .setParameter("member", member1)
+                    .getSingleResult();
 
-            // 컬렉션 페치 조인
-            String query = "select t from Team t join fetch t.members";
-
-            List<Team> result = em.createQuery(query, Team.class)
-                    .getResultList();
-
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("member = " + member);
-                }
-            }
+            System.out.println("findMember = " + findMember);
 
             tx.commit(); // 변경 내용을 db에 반영(플러시) -> 이 때 쿼리가 날아감
         } catch (Exception e) {
